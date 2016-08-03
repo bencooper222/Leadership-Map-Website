d1d03b5b28b4605e0341f5acad10dd68567a63e3
Benjamin Cooper <bencooper222@gmail.com>
2016-08-03 15:18:10 -0500

    Reconfigured settings to use config.json file

--- resources/graph.js
+++ resources/graph.js
@@ -1,117 +1,135 @@
 
-  function init() {
+var config;
 
-    // Finds the connections of each node
-    sigma.classes.graph.addMethod("neighbors", function(nodeId) {
-      var k,
-          neighbors = {},
-          index = this.allNeighborsIndex[nodeId] || {};
+loadJSON('resources/config.json',function(json){
+  config=JSON.parse(json);
+  console.log(config);
+  init();
+});
 
-      for (k in index)
-        neighbors[k] = this.nodesIndex[k];
+function init() {
 
-      return neighbors;
-    });
 
-   // Creates an instance of Sigma.js
-    var sigInst = new sigma({
-      renderers: [
-        {
-          container: document.getElementById("sigma-container"),
-          type: "canvas"
-        }
-      ]
-    });
+  // Finds the connections of each node
+  sigma.classes.graph.addMethod("neighbors", function (nodeId) {
+    var k,
+      neighbors = {},
+      index = this.allNeighborsIndex[nodeId] || {};
 
-    // Customizes its settings 
-    sigInst.settings({
-      // Drawing properties :
-      defaultLabelColor: "#000",
-      defaultLabelSize: 14,
-      defaultLabelHoverColor: "#fff",
-      labelThreshold: 11,
-      defaultHoverLabelBGColor: "#888",
-      defaultLabelBGColor: "#ddd",
-      defaultEdgeType: "straight",
-      defaultNodeColor: "blue",
+    for (k in index)
+      neighbors[k] = this.nodesIndex[k];
 
-      // Graph properties :
-      minNodeSize: 3,
-      maxNodeSize: 10,
-      minEdgeSize: 0.1,
-      maxEdgeSize: 0.2,
+    return neighbors;
+  });
 
-      // Mouse properties :
-      zoomMax: 20 
-    });
-    
-   
-
-    // Parses GEXF file to fill the graph
-    sigma.parsers.gexf(
-      "data/nonzeroedges with names.gexf",
-      sigInst,
-      function() {
-        //  Little hack here:
-        //  In the latest Sigma.js version have to delete edges" colors manually
-        sigInst.graph.edges().forEach(function(e) {
-          e.color = null;
-        });
-
-        // Also, to facilitate the update of node colors, store
-        // their original color under the key originalColor:
-        sigInst.graph.nodes().forEach(function(n) {
-          n.originalColor = n.color;
-        });
-
-        sigInst.refresh();
+  // Creates an instance of Sigma.js
+  var sigInst = new sigma({
+    renderers: [
+      {
+        container: document.getElementById("sigma-container"),
+        type: "canvas"
       }
-    );
+    ]
+  });
 
-    
-     // When a node is clicked, check for each node to see if it is connected. If not, set its color as gray
-     // Do the same for the edges
+  
+  // Customizes its settings 
+  sigInst.settings(config.sigmasettings);
 
-    var grayColor = "#ccc";
-    sigInst.bind("overNode", function(e) {
-      var nodeId = e.data.node.id,
-          toKeep = sigInst.graph.neighbors(nodeId);
-      toKeep[nodeId] = e.data.node;
 
-      sigInst.graph.nodes().forEach(function(n) {
-        if (toKeep[n.id])
-          n.color = n.originalColor;
-        else
-          n.color = grayColor;
-      });
 
-      sigInst.graph.edges().forEach(function(e) {
-        if (e.source === nodeId || e.target === nodeId)
-          e.color = null;
-        else
-          e.color = grayColor;
-      });
-
-    // Since the data has been modified, call the refresh method to make the colors update 
-      sigInst.refresh();
-    });
-
-    // When a node is no longer being hovered over, return to original colors
-    sigInst.bind("outNode", function(e) {
-      sigInst.graph.nodes().forEach(function(n) {
-        n.color = n.originalColor;
-      });
-
-      sigInst.graph.edges().forEach(function(e) {
+  // Parses GEXF file to fill the graph
+  sigma.parsers.gexf(
+    config.datalocation,
+    sigInst,
+    function () {
+      //  Little hack here:
+      //  In the latest Sigma.js version have to delete edges" colors manually
+      sigInst.graph.edges().forEach(function (e) {
         e.color = null;
       });
 
-      sigInst.refresh();
-    });
-  }
+      // Also, to facilitate the update of node colors, store
+      // their original color under the key originalColor:
+      sigInst.graph.nodes().forEach(function (n) {
+        n.originalColor = n.color;
+      });
 
-  if (document.addEventListener)
-    document.addEventListener("DOMContentLoaded", init, false);
-  else
-    window.onload = init;
-  
+      sigInst.refresh();
+    }
+  );
+
+
+  // When a node is clicked, check for each node to see if it is connected. If not, set its color as gray
+  // Do the same for the edges
+
+  var grayColor = "#ccc";
+  sigInst.bind("overNode", function (e) {
+    var nodeId = e.data.node.id,
+      toKeep = sigInst.graph.neighbors(nodeId);
+    toKeep[nodeId] = e.data.node;
+
+    sigInst.graph.nodes().forEach(function (n) {
+      if (toKeep[n.id])
+        n.color = n.originalColor;
+      else
+        n.color = grayColor;
+    });
+
+    sigInst.graph.edges().forEach(function (e) {
+      if (e.source === nodeId || e.target === nodeId)
+        e.color = null;
+      else
+        e.color = grayColor;
+    });
+
+    // Since the data has been modified, call the refresh method to make the colors update 
+    sigInst.refresh();
+  });
+
+  // When a node is no longer being hovered over, return to original colors
+  sigInst.bind("outNode", function (e) {
+    sigInst.graph.nodes().forEach(function (n) {
+      n.color = n.originalColor;
+    });
+
+    sigInst.graph.edges().forEach(function (e) {
+      e.color = null;
+    });
+
+    sigInst.refresh();
+  });
+}
+
+
+
+// Loads a config.json file that directs to the location of various files
+ function loadJSON(location,callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', location, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ 
+
+
+
+
+
+
+/*
+
+if (document.addEventListener)
+  document.addEventListener("DOMContentLoaded", init, false);
+else
+console.log("hi")
+ // window.onload = init;
+ */
