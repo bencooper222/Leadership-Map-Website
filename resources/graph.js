87924fb2f2d5a3f952fbac6f8733e0d55e292141
Benjamin Cooper <bencooper222@gmail.com>
2016-08-09 00:49:46 -0500

    Added filtering functionality

--- resources/graph.js
+++ resources/graph.js
@@ -1,8 +1,10 @@
 
 var config;
+var sigInst;
+var filter;
 
-loadJSON('resources/config.json',function(json){
-  config=JSON.parse(json);
+loadJSON('resources/config.json', function (json) {
+  config = JSON.parse(json);
   console.log(config);
   init();
 });
@@ -24,20 +26,37 @@ function init() {
   });
 
   // Creates an instance of Sigma.js
-  var sigInst = new sigma({
+  sigInst = new sigma({
     renderers: [
       {
         container: document.getElementById("sigma-container"),
         type: "canvas"
- }
+      }
     ]
   });
 
-  
+
   // Customizes its settings 
-  sigInst.settings(config.sigmasettings);
+  //  sigInst.settings(config.sigmasettings);
 
-
+  sigInst.settings({
+    // Drawing properties :
+    defaultNodeColor: "#005596",
+    defaultLabelColor: "#000",
+    defaultLabelSize: 14,
+    defaultLabelHoverColor: "#fff",
+    labelThreshold: 11,
+    defaultHoverLabelBGColor: "#888",
+    defaultLabelBGColor: "#ddd",
+    defaultEdgeType: "straight",
+    // Graph properties :
+    minNodeSize: 3,
+    maxNodeSize: 10,
+    minEdgeSize: 0.1,
+    maxEdgeSize: 0.2,
+    // Mouse properties :
+    zoomMax: 20
+  });
 
   // Parses GEXF file to fill the graph
   sigma.parsers.gexf(
@@ -60,7 +79,7 @@ function init() {
     }
   );
 
-     
+
 
 
   // When a node is clicked, check for each node to see if it is connected. If not, set its color as gray
@@ -102,26 +121,52 @@ function init() {
 
     sigInst.refresh();
   });
+
+  // instatiating filter
+  filter = new sigma.plugins.filter(sigInst);
+  sigInst.refresh();
+}
+
+//TODO: set up filter.js
+function filterEdgeWeight(value) {
+  filter.edgesBy(function (e) {
+    return e.weight > value;
+  }, 'edge-weight').apply();
+}
+
+function verifyEnter(event) {
+  var obj = event.target;
+  if (!event) event = window.event;
+  var keyCode = event.keyCode || event.which;
+  if (keyCode == '13') {
+    // Enter pressed
+
+    switch (obj.id) {
+      case "edgeWeight":
+        filter.undo('edge-weight').apply();
+        filterEdgeWeight(parseInt(obj.value));
+        break;
+    }
+
+  }
+}
+
+// Loads a config.json file that directs to the location of various files
+function loadJSON(location, callback) {
+
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', location, true); // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
 
 
-
-// Loads a config.json file that directs to the location of various files
- function loadJSON(location,callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', location, true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
-
- 
 
 
 
