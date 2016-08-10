877519ea292fdc4943fe7c6d6ebe34ce2d3ae30f
Benjamin Cooper <bencooper222@gmail.com>
2016-08-09 19:47:17 -0500

    Reconfigured resize() function and added organization list

--- resources/graph.js
+++ resources/graph.js
@@ -9,6 +9,16 @@ loadJSON('resources/config.json', function (json) {
   init();
 });
 
+function resizeContainers() {
+  var sigContainer = document.getElementById("sigma-container");
+  var sideBar = document.getElementById("sidebar");
+
+  //alter leftness and width of sigma container
+  sigContainer.style.left = sideBar.offsetWidth + "px";
+  sigContainer.style.width = (document.body.offsetWidth - sideBar.offsetWidth) + "px";
+  sigInst.refresh();
+}
+
 function init() {
 
 
@@ -37,27 +47,27 @@ function init() {
 
 
   // Customizes its settings 
-  //  sigInst.settings(config.sigmasettings);
-
-  sigInst.settings({
-    // Drawing properties :
-    defaultNodeColor: "#005596",
-    defaultLabelColor: "#000",
-    defaultLabelSize: 14,
-    defaultLabelHoverColor: "#fff",
-    labelThreshold: 11,
-    defaultHoverLabelBGColor: "#888",
-    defaultLabelBGColor: "#ddd",
-    defaultEdgeType: "straight",
-    // Graph properties :
-    minNodeSize: 3,
-    maxNodeSize: 10,
-    minEdgeSize: 0.1,
-    maxEdgeSize: 0.2,
-    // Mouse properties :
-    zoomMax: 20
-  });
-
+  sigInst.settings(config.sigmasettings);
+  /*
+    sigInst.settings({
+      // Drawing properties :
+      defaultNodeColor: "#005596",
+      defaultLabelColor: "#000",
+      defaultLabelSize: 14,
+      defaultLabelHoverColor: "#fff",
+      labelThreshold: 11,
+      defaultHoverLabelBGColor: "#888",
+      defaultLabelBGColor: "#ddd",
+      defaultEdgeType: "straight",
+      // Graph properties :
+      minNodeSize: 3,
+      maxNodeSize: 10,
+      minEdgeSize: 0.1,
+      maxEdgeSize: 0.2,
+      // Mouse properties :
+      zoomMax: 20
+    });
+  */
   // Parses GEXF file to fill the graph
   sigma.parsers.gexf(
     config.datalocation,
@@ -70,9 +80,12 @@ function init() {
 
       });
 
-      // Also, to facilitate the update of node colors, store
-      // their original color under the key originalColor:
+
       sigInst.graph.nodes().forEach(function (n) {
+        // convert XML attribute string to an array
+        n.attributes.organizations = n.attributes.organizations.split(", ");
+        // Also, to facilitate the update of node colors, store
+        // their original color under the key originalColor:
         n.originalColor = n.color;
       });
 
@@ -87,28 +100,35 @@ function init() {
   // Do the same for the edges
 
   var grayColor = "#ccc";
-  sigInst.bind("overNode", function (e) {
-    var nodeId = e.data.node.id,
-      toKeep = sigInst.graph.neighbors(nodeId);
-    toKeep[nodeId] = e.data.node;
+  sigInst.bind("overNode",
+    function (e) {
+      var nodeId = e.data.node.id,
+        toKeep = getFilteredNeighrbors(nodeId);
+      // toKeep[nodeId] = e.data.node;
+      console.log(e.data.node);
 
-    sigInst.graph.nodes().forEach(function (n) {
-      if (toKeep[n.id])
-        n.color = n.originalColor;
-      else
-        n.color = grayColor;
+      sigInst.graph.nodes().forEach(function (n) {
+
+        if (toKeep[n.id]) {
+
+          n.color = n.originalColor;
+        }
+        else {
+
+          n.color = grayColor;
+        }
+      });
+
+      sigInst.graph.edges().forEach(function (e) {
+        if (e.source === nodeId || e.target === nodeId)
+          e.color = null;
+        else
+          e.color = grayColor;
+      });
+
+      // Since the data has been modified, call the refresh method to make the colors update 
+      sigInst.refresh();
     });
-
-    sigInst.graph.edges().forEach(function (e) {
-      if (e.source === nodeId || e.target === nodeId)
-        e.color = null;
-      else
-        e.color = grayColor;
-    });
-
-    // Since the data has been modified, call the refresh method to make the colors update 
-    sigInst.refresh();
-  });
 
   // When a node is no longer being hovered over, return to original colors
   sigInst.bind("outNode", function (e) {
@@ -133,7 +153,31 @@ function filterEdgeWeight(value) {
   filter.edgesBy(function (e) {
     return e.weight > value;
   }, 'edge-weight').apply();
+
 }
+
+function getFilteredNeighrbors(nodeId) {
+  var neighbors = sigInst.graph.neighbors(nodeId);
+  var validNodes = [];
+
+  sigInst.graph.edges().forEach(function (e) {
+    //only count visible incident edges for determining neighborhoods of nodeId
+    if (e.hidden === false || typeof e.hidden === "undefined") { // if filtering hasn't started, e.hidden will be undefined 
+      if (e.source === nodeId) {
+        validNodes[e.target] = sigInst.graph.nodes(e.target);
+      }
+      if (e.target === nodeId) {
+        validNodes[e.source] = sigInst.graph.nodes(e.source);
+      }
+    }
+
+
+  });
+
+  validNodes[nodeId] = sigInst.graph.nodes(nodeId); // add the node with id nodeId itself
+  return validNodes;
+}
+
 
 function verifyEnter(event) {
   var obj = event.target;
@@ -145,7 +189,7 @@ function verifyEnter(event) {
     switch (obj.id) {
       case "edgeWeight":
         filter.undo('edge-weight').apply();
-        filterEdgeWeight(parseInt(obj.value));
+        filterEdgeWeight(parseFloat(obj.value));
         break;
     }
 
